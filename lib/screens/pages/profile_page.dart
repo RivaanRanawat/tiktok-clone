c0b9d3844aac33662ffb208ecbae9ea7b40bfe68
Rivaan Ranawat <namanrivaan@gmail.com>
2020-10-26 14:25:11 +0530

    added followers following functions

--- lib/screens/pages/profile_page.dart
+++ lib/screens/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tiktok_clone/universal_variables.dart';
@@ -12,11 +13,54 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   String username;
-  String onlineUser;
+  String currentUser;
   String profilePic;
   Future myVideos;
   var likes = 0;
   var isData = false;
+  var isFollowing = false;
+  int following;
+  int followers;
+  TextEditingController usernameController = TextEditingController();
+
+  followUser() async {
+    var doc = await userCollection
+        .doc(widget.uid)
+        .collection("followers")
+        .doc(currentUser)
+        .get();
+    if (!doc.exists) {
+      userCollection
+          .doc(widget.uid)
+          .collection("followers")
+          .doc(currentUser)
+          .set({});
+      userCollection
+          .doc(currentUser)
+          .collection("following")
+          .doc(widget.uid)
+          .set({});
+      setState(() {
+        isFollowing = true;
+        followers++;
+      });
+    } else {
+      userCollection
+          .doc(widget.uid)
+          .collection("followers")
+          .doc(currentUser)
+          .delete();
+      userCollection
+          .doc(currentUser)
+          .collection("following")
+          .doc(widget.uid)
+          .delete();
+      setState(() {
+        isFollowing = false;
+        followers--;
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -30,13 +74,82 @@ class _ProfilePageState extends State<ProfilePage> {
     DocumentSnapshot userDoc = await userCollection.doc(widget.uid).get();
     username = userDoc.data()["username"];
     profilePic = userDoc.data()["profilePic"];
+    currentUser = FirebaseAuth.instance.currentUser.uid;
 
     var docs = await videoColection.where("uid", isEqualTo: widget.uid).get();
     for (var item in docs.docs) {
       likes += item.data()["likes"].length;
     }
+    var followerDoc =
+        await userCollection.doc(widget.uid).collection("followers").get();
+    var followingDoc =
+        await userCollection.doc(widget.uid).collection("following").get();
+    followers = followerDoc.docs.length;
+    following = followingDoc.docs.length;
+
+    userCollection
+        .doc(widget.uid)
+        .collection("followers")
+        .doc(currentUser)
+        .get()
+        .then((value) {
+      if (value.exists) {
+        setState(() {
+          isFollowing = true;
+        });
+      } else {
+        setState(() {
+          isFollowing = false;
+        });
+      }
+    });
     setState(() {
       isData = true;
+    });
+  }
+
+  editProfile() {
+    return showDialog(context: context, builder: (context){
+      return Dialog(child: Container(
+        height: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Edit Profile", style: latoStyle(20, Colors.red),),
+            SizedBox(height: 20,),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  hintStyle: ralewayStyle(17, Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            InkWell(
+              onTap: (){
+                userCollection.doc(currentUser).update({
+                  "username": usernameController.text
+                });
+                setState(() {
+                  username = usernameController.text;
+                });
+                Navigator.of(context).pop();
+              },
+                          child: Container(
+                width: MediaQuery.of(context).size.width/2,
+                height: 50,
+                color: Colors.red,
+                child: Center(
+                  child: Text("Edit", style: ralewayStyle(20),),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),);
     });
   }
 
@@ -81,11 +194,11 @@ class _ProfilePageState extends State<ProfilePage> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Text(
-                          "100",
+                          followers.toString(),
                           style: latoStyle(23, Colors.red, FontWeight.bold),
                         ),
                         Text(
-                          "50",
+                          following.toString(),
                           style: latoStyle(23, Colors.red, FontWeight.bold),
                         ),
                         Text(
@@ -117,21 +230,43 @@ class _ProfilePageState extends State<ProfilePage> {
                     SizedBox(
                       height: 30,
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 2,
-                      height: 40,
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          "Edit Profile",
-                          style: GoogleFonts.montserrat(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                    currentUser == widget.uid
+                        ? Container(
+                            width: MediaQuery.of(context).size.width / 2,
+                            height: 40,
+                            color: Colors.red,
+                            child: InkWell(
+                              onTap: () => editProfile(),
+                              child: Center(
+                                child: Text(
+                                  "Edit Profile",
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        : Container(
+                            width: MediaQuery.of(context).size.width / 2,
+                            height: 40,
+                            color: Colors.red,
+                            child: InkWell(
+                              onTap: () => followUser(),
+                              child: Center(
+                                child: Text(
+                                  isFollowing == false ? "Follow" : "Unfollow",
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
                     SizedBox(
                       height: 20,
                     ),
